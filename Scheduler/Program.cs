ee3f1f2304034abc74e554099946777ee4c6f16d
Alex <al.s.dudkin@gmail.com>
2026-06-20 18:18:26 +0300

    Fix registration in scheduler

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -1,6 +1,11 @@
+using Application;
+using Infrastructure;
 using Scheduler;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.RegisterInfrastructure();
+builder.Services.RegisterApplication();
 builder.Services.AddHostedService<CityWeatherWorker>();
 
 var host = builder.Build();
