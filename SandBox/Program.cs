5ce5ce2ca74bbe18437821f88a509456a75fcf50
Alex <al.s.dudkin@gmail.com>
2026-06-22 08:48:05 +0300

    Api Client fix

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -2,17 +2,19 @@
 
 var builder = Host.CreateApplicationBuilder();
 
-builder.Services.AddHttpClient<Weather>();
+builder.Services.AddHttpClient<ApiV1>();
 
 var host = builder.Build();
 
-var weather = host.Services.GetService<Weather>();
+var weather = host.Services.GetService<ApiV1>();
 weather.BaseUrl = builder.Configuration["Api"];
 
 var cities = await weather.CityAllAsync(string.Empty);
 foreach (var city in cities)
 {
     Console.WriteLine($"{city.Id} {city.Name}");
+    var current = await weather.WeatherAsync(city.Id);
+    Console.WriteLine($"{current.Timestamp} {current.Temperature}");
     var histories = await weather.HistoryAsync(city.Id);
     foreach (var history in histories.History)
         Console.WriteLine($"{history.Timestamp} {history.Temperature}");
